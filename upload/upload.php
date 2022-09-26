b289d254e3ea2d48a3205f4f7c0a12192b267bb0
Nattawat <78522669+Panlawan@users.noreply.github.com>
2022-09-26 23:57:45 +0700

    Add files via upload

---
+++ upload/upload.php
@@ -0,0 +1,35 @@
+<?php
+$target_dir = "/var/www/downloads/upload/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+
+// Check if image file is a actual image or fake image
+if(isset($_POST['submit'])){
+  if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+  }
+  else
+  {
+    // Check if file already exists
+    if (file_exists($target_file)) {
+      echo "Sorry, file already exists.";
+      $uploadOk = 0;
+    }
+    else
+    {
+      $uploadOk = 1;
+    }
+  }
+} 
+
+if ($uploadOk == 1) {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
+  {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } 
+  else 
+  {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
